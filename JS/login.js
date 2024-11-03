ff3a84508de9ba0a76c544666db73ff7c7b89f4d
si-rak <zebeaman3@gmail.com>
2024-11-03 18:19:01 +0100

    login, register css edit

--- JS/login.js
+++ JS/login.js
@@ -12,7 +12,7 @@ document.getElementById('loginButton').addEventListener('click', function(event)
   const errorMessage = document.getElementById('errorMessage');
   errorMessage.style.display = 'none'; 
 
-  if (email === adminEmail && password === adminPassword) {
+  if (email === login@admin.com && password === admin357) {
       localStorage.setItem('', ''); 
       
       window.location.href = 'https://67119a09e46c88e543d6878c--warm-buttercream-0d2533.netlify.app/html/edit'; 
